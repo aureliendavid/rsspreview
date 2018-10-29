a83dad4fe09ec8abf950223ec30dc9d528a2e78b
Aurelien David <aurelien.david@telecom-paristech.fr>
2018-10-29 21:40:19 +0100

    don't cache modified requests

--- background.js
+++ background.js
@@ -3,11 +3,29 @@ function detectFeed(event) {
 
 	// force application/rss+xml to text/xml so the browser displays it instead of downloading
 
+	var isfeed = false;
+
 	for (var header of event.responseHeaders) {
 	    if (header.name.toLowerCase() == "content-type") {
 			header.value = header.value.replace(/application\/(rss|atom)\+xml/,'text/xml');
+			isfeed = true;
 			break;
 	    }
+	}
+
+	if (isfeed) {
+
+		for (var i = 0; i < event.responseHeaders.length; i++) {
+		    if (event.responseHeaders[i].name.toLowerCase() == "cache-control") {
+				event.responseHeaders.splice(i, 1);
+				break;
+		    }
+		}
+
+		// don't cache requests we modified
+		// otherwise on reload the content-type won't be modified again
+		event.responseHeaders.push({ name : "Cache-Control", value : "no-cache, no-store, must-revalidate" });
+
 	}
 
     return {
