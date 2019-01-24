a7f23af160c6b0bb97c380c3c0667e9e786c177a
Aurelien David <aurelien.david@telecom-paristech.fr>
2019-01-24 22:58:54 +0100

    use browser style for popup

--- popup/popup.js
+++ popup/popup.js
@@ -7,18 +7,33 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const feeds = JSON.parse(url.searchParams.get('feeds'));
 
   for (feed_url in feeds) {
-  	if (feeds.hasOwnProperty(feed_url)) {
-  		let li = document.createElement("li");
-  		let a = document.createElement("a");
+    if (feeds.hasOwnProperty(feed_url)) {
 
-  		a.href = feed_url;
-  		a.target = "_blank";
-  		a.innerText = feeds[feed_url];
+      let li = document.createElement("div");
+      li.classList.add("panel-list-item");
+      li.setAttribute("data-href", feed_url);
 
-  		li.appendChild(a);
-  		feedList.appendChild(li);
-  	}
+      let a = document.createElement("div");
+      a.classList.add("text");
+      a.innerText = feeds[feed_url];
+
+      li.appendChild(a);
+      feedList.appendChild(li);
+    }
   }
+
+  document.querySelectorAll(".panel-list-item").forEach( (elem) => {
+
+    elem.addEventListener('click', (event) => {
+
+      let url = elem.getAttribute("data-href");
+      if (url)
+        browser.tabs.create({url: url});
+
+
+    });
+
+  });
 
 });
 
