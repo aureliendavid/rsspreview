be3c285863bc44d23e2263be4f6fb1f168c520bf
Alessandro Amella <44754837+alessandroamella@users.noreply.github.com>
2026-01-11 02:17:07 +0100

    feat: add dark theme support and format files

--- popup/popup.js
+++ popup/popup.js
@@ -1,80 +1,58 @@
-
 var android = false;
 
 browser.runtime.getPlatformInfo().then((info) => {
-  android = info.os == "android"
+  android = info.os === "android";
 });
 
-
-document.addEventListener("DOMContentLoaded", function(event) {
-
-
-  const feedList = document.getElementById('feedList');
+document.addEventListener("DOMContentLoaded", (_event) => {
+  const feedList = document.getElementById("feedList");
 
   const url = new URL(location.href);
   // `+` converts the string to an number
-  const tabId = +url.searchParams.get('tabId');
-  const feeds = JSON.parse(url.searchParams.get('feeds'));
+  const tabId = +url.searchParams.get("tabId");
+  const feeds = JSON.parse(url.searchParams.get("feeds"));
 
   browser.runtime.getPlatformInfo().then((info) => {
-  android = info.os == "android";
+    android = info.os === "android";
 
-  for (feed_url in feeds) {
-    if (feeds.hasOwnProperty(feed_url)) {
+    for (feed_url in feeds) {
+      if (Object.hasOwn(feeds, feed_url)) {
+        const li = document.createElement("div");
+        li.classList.add("panel-list-item");
+        li.setAttribute("data-href", feed_url);
 
-      let li = document.createElement("div");
-      li.classList.add("panel-list-item");
-      li.setAttribute("data-href", feed_url);
+        const a = document.createElement("div");
+        a.classList.add("text");
+        a.innerText = feeds[feed_url];
 
-      let a = document.createElement("div");
-      a.classList.add("text");
-      a.innerText = feeds[feed_url];
+        li.appendChild(a);
 
-      li.appendChild(a);
+        if (android) li.classList.add("android-feed-btn");
 
-      if (android)
-        li.classList.add("android-feed-btn");
-
-      feedList.appendChild(li);
-    }
-  }
-
-
-
-
-  browser.storage.sync.get({newTab: !android}).then(function(options) {
-
-  document.querySelectorAll(".panel-list-item").forEach( (elem) => {
-
-    function onUpdated(tab) {
-    }
-
-    function onError(error) {
-    }
-
-    elem.addEventListener('click', (event) => {
-
-      let url = elem.getAttribute("data-href");
-      if (url) {
-        if (options.newTab) {
-          var params = { url: url } ;
-          if (!android) {
-            params.openerTabId = tabId ;
-          }
-          browser.tabs.create(params);
-        }
-        else
-          browser.tabs.update({url: url}).then(onUpdated, onError);
+        feedList.appendChild(li);
       }
-      if (android)
-        window.close();
+    }
 
-    });
+    browser.storage.sync.get({ newTab: !android }).then((options) => {
+      document.querySelectorAll(".panel-list-item").forEach((elem) => {
+        function onUpdated(_tab) {}
 
-  }); // end forall
+        function onError(_error) {}
 
-  }); // end options
-
+        elem.addEventListener("click", (_event) => {
+          const url = elem.getAttribute("data-href");
+          const params = { url: url };
+          if (url) {
+            if (options.newTab) {
+              if (!android) {
+                params.openerTabId = tabId;
+              }
+              browser.tabs.create(params);
+            } else browser.tabs.update({ url: url }).then(onUpdated, onError);
+          }
+          if (android) window.close();
+        });
+      }); // end forall
+    }); // end options
   }); // and getplatform
-
 });
--- rsspreview.js
+++ rsspreview.js
@@ -1,15 +1,20 @@
-(function() {
+(() => {
   /**
    * Check and set a global guard variable.
    * If this content script is injected into the same page again,
    * it will do nothing next time.
    */
   if (window.hasRun) {
-    console.log('already run');
+    console.log("already run");
     return;
   }
 
   window.hasRun = true;
+
+  // Detect system theme for the initial default
+  const systemDarkDefault = window.matchMedia(
+    "(prefers-color-scheme: dark)",
+  ).matches;
 
   // defaults
   var options = {
@@ -20,26 +25,27 @@
     preventPreview: false,
     fullPreview: false,
     doAuthor: false,
+    darkTheme: systemDarkDefault, // DYNAMIC DEFAULT
     enableCss: false,
     bypassCSP: false,
     customCss: null,
-    newTab: true
+    newTab: true,
   };
 
-  let xml_parser = new XMLSerializer();
-  let html_parser = new DOMParser();
+  const xml_parser = new XMLSerializer();
+  const html_parser = new DOMParser();
 
   function xhrdoc(url, type, cb) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
 
-    xhr.responseType = 'document';
-    xhr.overrideMimeType('text/' + type);
+    xhr.responseType = "document";
+    xhr.overrideMimeType(`text/${type}`);
 
     xhr.onload = () => {
       if (xhr.readyState === xhr.DONE) {
         if (xhr.status === 200) {
-          let resp = type == 'xml' ? xhr.responseXML : xhr.response;
+          const resp = type === "xml" ? xhr.responseXML : xhr.response;
           cb(resp);
         }
       }
@@ -49,11 +55,11 @@
   }
 
   function applyxsl(xmlin, xsl, node, doc = document) {
-    let xsltProcessor = new XSLTProcessor();
+    const xsltProcessor = new XSLTProcessor();
     xsltProcessor.importStylesheet(xsl);
-    xsltProcessor.setParameter(null, 'fullPreview', options.fullPreview);
-    xsltProcessor.setParameter(null, 'doAuthor', options.doAuthor);
-    let fragment = xsltProcessor.transformToFragment(xmlin, doc);
+    xsltProcessor.setParameter(null, "fullPreview", options.fullPreview);
+    xsltProcessor.setParameter(null, "doAuthor", options.doAuthor);
+    const fragment = xsltProcessor.transformToFragment(xmlin, doc);
     node.appendChild(fragment);
   }
 
@@ -63,15 +69,15 @@
 
   function formatsubtitle() {
     try {
-      let feed_desc = document.getElementById('feedSubtitleRaw');
+      const feed_desc = document.getElementById("feedSubtitleRaw");
 
-      let html_desc = html_parser.parseFromString(
-        '<h2 id="feedSubtitleText">' + feed_desc.innerText + '</h2>',
-        'text/html'
+      const html_desc = html_parser.parseFromString(
+        `<h2 id="feedSubtitleText">${feed_desc.innerText}</h2>`,
+        "text/html",
       );
-      let xml_desc = xml_parser.serializeToString(html_desc.body.firstChild);
+      const xml_desc = xml_parser.serializeToString(html_desc.body.firstChild);
 
-      feed_desc.insertAdjacentHTML('afterend', xml_desc);
+      feed_desc.insertAdjacentHTML("afterend", xml_desc);
 
       feed_desc.parentNode.removeChild(feed_desc);
     } catch (e) {
@@ -81,60 +87,59 @@
 
   function formatdescriptions(el = document) {
     // unescapes descriptions to html then to xml
-    let tohtml = el.getElementsByClassName('feedRawContent');
+    const tohtml = el.getElementsByClassName("feedRawContent");
 
     for (let i = 0; i < tohtml.length; i++) {
-
       try {
-        let html_txt = '';
-        if (tohtml[i].getAttribute('desctype') == 'text/plain') {
-          html_txt = '<div class="feedEntryContent" style="white-space: pre-wrap;" >' + tohtml[i].innerHTML + '</div>';
-        }
-        else if (tohtml[i].getAttribute('desctype') == 'xhtml') {
-          html_txt = '<div class="feedEntryContent">' + tohtml[i].innerHTML + '</div>';
-        }
-        else {
-          html_txt = '<div class="feedEntryContent">' + tohtml[i].textContent + '</div>';
+        let html_txt = "";
+        if (tohtml[i].getAttribute("desctype") === "text/plain") {
+          html_txt =
+            '<div class="feedEntryContent" style="white-space: pre-wrap;" >' +
+            tohtml[i].innerHTML +
+            "</div>";
+        } else if (tohtml[i].getAttribute("desctype") === "xhtml") {
+          html_txt = `<div class="feedEntryContent">${tohtml[i].innerHTML}</div>`;
+        } else {
+          html_txt = `<div class="feedEntryContent">${tohtml[i].textContent}</div>`;
         }
 
-        let html_desc = html_parser.parseFromString(html_txt, 'text/html');
-        let xml_desc = xml_parser.serializeToString(
-          html_desc.body.firstChild
+        const html_desc = html_parser.parseFromString(html_txt, "text/html");
+        const xml_desc = xml_parser.serializeToString(
+          html_desc.body.firstChild,
         );
 
-        tohtml[i].insertAdjacentHTML('afterend', xml_desc);
-        tohtml[i].setAttribute('todel', 1);
+        tohtml[i].insertAdjacentHTML("afterend", xml_desc);
+        tohtml[i].setAttribute("todel", 1);
       } catch (e) {
         console.error(e);
         console.log(tohtml[i]);
       }
-
     }
 
-    el.querySelectorAll('.feedRawContent').forEach(a => {
-      if (a.getAttribute('todel') == '1') {
+    el.querySelectorAll(".feedRawContent").forEach((a) => {
+      if (a.getAttribute("todel") === "1") {
         a.remove();
       }
     });
   }
 
   function removeemptyenclosures(el = document) {
-    let encs = el.getElementsByClassName('enclosures');
+    const encs = el.getElementsByClassName("enclosures");
 
     for (let i = 0; i < encs.length; i++)
-      if (!encs[i].firstChild) encs[i].style.display = 'none';
+      if (!encs[i].firstChild) encs[i].style.display = "none";
   }
 
   function formatfilenames(el = document) {
-    let encfn = el.getElementsByClassName('enclosureFilename');
+    const encfn = el.getElementsByClassName("enclosureFilename");
 
     for (let i = 0; i < encfn.length; i++) {
-      let url = new URL(encfn[i].innerText);
+      const url = new URL(encfn[i].innerText);
 
       if (url) {
-        let fn = url.pathname.split('/').pop();
+        const fn = url.pathname.split("/").pop();
 
-        if (fn != '') encfn[i].innerText = fn;
+        if (fn !== "") encfn[i].innerText = fn;
       }
     }
   }
@@ -143,34 +148,36 @@
     function humanfilesize(size) {
       let i = 0;
 
-      if (size && size != '' && size > 0)
+      if (size && size !== "" && size > 0)
         i = Math.floor(Math.log(size) / Math.log(1024));
 
       return (
-        (size / Math.pow(1024, i)).toFixed(2) * 1 +
-        ' ' +
-        ['B', 'kB', 'MB', 'GB', 'TB'][i]
+        (size / 1024 ** i).toFixed(2) * 1 +
+        " " +
+        ["B", "kB", "MB", "GB", "TB"][i]
       );
     }
 
-    let encsz = el.getElementsByClassName('enclosureSize');
+    const encsz = el.getElementsByClassName("enclosureSize");
     for (let i = 0; i < encsz.length; i++) {
-      let hsize = humanfilesize(encsz[i].innerText);
+      const hsize = humanfilesize(encsz[i].innerText);
 
       if (hsize) encsz[i].innerText = hsize;
     }
   }
 
   function formattitles(el = document) {
-    let et = el.getElementsByClassName('entrytitle');
+    const et = el.getElementsByClassName("entrytitle");
 
     for (let i = 0; i < et.length; i++) {
       //basically removes html content if there is some
       //only do it if there's a tag to avoid doing it when text titles cointain a '&'
       //(which can be caught but still displays an error in console, which is annoying)
-      if (et[i].innerText.indexOf('<') >= 0 || et[i].innerText.indexOf('&amp;')) {
-
-        let tmp = document.createElement('span');
+      if (
+        et[i].innerText.indexOf("<") >= 0 ||
+        et[i].innerText.indexOf("&amp;")
+      ) {
+        const tmp = document.createElement("span");
         try {
           tmp.innerHTML = et[i].innerText;
           et[i].innerText = tmp.textContent;
@@ -184,71 +191,70 @@
   }
 
   function formatdates(el = document) {
-    let lang = getlang();
+    const lang = getlang();
     if (!lang) return;
 
-    let opts = {
-      weekday: 'long',
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
+    const opts = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
     };
 
-    let ed = el.getElementsByClassName('lastUpdated');
+    const ed = el.getElementsByClassName("lastUpdated");
     for (let i = 0; i < ed.length; i++) {
-      let d = new Date(ed[i].innerText);
-      if (isNaN(d)) continue;
+      const d = new Date(ed[i].innerText);
+      if (Number.isNaN(d)) continue;
 
-      let dstr =
-        d.toLocaleDateString(lang, opts) + ' ' + d.toLocaleTimeString(lang);
+      const dstr = `${d.toLocaleDateString(lang, opts)} ${d.toLocaleTimeString(lang)}`;
 
       ed[i].innerText = dstr;
     }
 
-    let lu = el.getElementById('feedLastUpdate');
-    if (lu && lu.innerText.trim() != "") {
-      lu.innerText = "Last updated: " + lu.innerText;
+    const lu = el.getElementById("feedLastUpdate");
+    if (lu && lu.innerText.trim() !== "") {
+      lu.innerText = `Last updated: ${lu.innerText}`;
     }
   }
 
   function extensionimages(el = document) {
-    let extimgs = el.getElementsByClassName('extImg');
+    const extimgs = el.getElementsByClassName("extImg");
 
     for (let i = 0; i < extimgs.length; i++)
       extimgs[i].src = chrome.runtime.getURL(
-        extimgs[i].attributes['data-src'].nodeValue
+        extimgs[i].attributes["data-src"].nodeValue,
       );
   }
 
   function applysettings() {
-
-    document.querySelectorAll('.mediaThumb').forEach((elem) => {
+    document.querySelectorAll(".mediaThumb").forEach((elem) => {
       elem.style.display = options.doThumb ? "block" : "none";
     });
 
-
-    document.querySelectorAll('img').forEach((elem) => {
-      if (options.doMaxWidth)
-        elem.style["max-width"] = options.valMaxWidth;
+    document.querySelectorAll("img").forEach((elem) => {
+      if (options.doMaxWidth) elem.style["max-width"] = options.valMaxWidth;
     });
-
   }
 
   function makepreviewhtml() {
-    let doc = document.implementation.createHTMLDocument('');
+    const doc = document.implementation.createHTMLDocument("");
     doc.body.id = "rsspreviewBody";
 
-    let feedBody = doc.createElement('div');
-    feedBody.id = 'feedBody';
+    if (options.darkTheme) {
+      doc.documentElement.classList.add("dark");
+    }
+
+    const feedBody = doc.createElement("div");
+    feedBody.id = "feedBody";
     doc.body.appendChild(feedBody);
 
-    let css = doc.createElement('link');
-    css.setAttribute('rel', 'stylesheet');
-    css.setAttribute('href', chrome.runtime.getURL('preview.css'));
+    const css = doc.createElement("link");
+    css.setAttribute("rel", "stylesheet");
+    css.setAttribute("href", chrome.runtime.getURL("preview.css"));
     doc.head.appendChild(css);
 
     if (options.enableCss && options.customCss) {
-      let node = doc.createElement('style');
+      const node = doc.createElement("style");
       node.innerHTML = options.customCss;
       doc.head.appendChild(node);
     }
@@ -260,24 +266,24 @@
     let rootNode = document.getRootNode();
 
     // for chrome
-    let d = document.getElementById('webkit-xml-viewer-source-xml');
-    if (d && d.firstChild) rootNode = d.firstChild;
+    const d = document.getElementById("webkit-xml-viewer-source-xml");
+    if (d?.firstChild) rootNode = d.firstChild;
 
     const rootName = rootNode.documentElement.nodeName.toLowerCase();
 
     let isRSS1 = false;
 
-    if (rootName == 'rdf' || rootName == 'rdf:rdf') {
-      if (rootNode.documentElement.attributes['xmlns']) {
-        isRSS1 = rootNode.documentElement.attributes['xmlns'].nodeValue.search('rss') > 0;
+    if (rootName === "rdf" || rootName === "rdf:rdf") {
+      if (rootNode.documentElement.attributes.xmlns) {
+        isRSS1 =
+          rootNode.documentElement.attributes.xmlns.nodeValue.search("rss") > 0;
       }
-
     }
 
     if (
-      rootName == 'rss' ||
-      rootName == 'channel' || // rss2
-      rootName == 'feed' || // atom
+      rootName === "rss" ||
+      rootName === "channel" || // rss2
+      rootName === "feed" || // atom
       isRSS1
     )
       return rootNode;
@@ -286,19 +292,19 @@
   }
 
   function main(feedNode) {
-    let feed_url = window.location.href;
-    let preview = makepreviewhtml();
+    const _feed_url = window.location.href;
+    const preview = makepreviewhtml();
 
-    xhrdoc(chrome.runtime.getURL('rss.xsl'), 'xml', xsl_xml => {
-      applyxsl(feedNode, xsl_xml, preview.getElementById('feedBody'), preview);
+    xhrdoc(chrome.runtime.getURL("rss.xsl"), "xml", (xsl_xml) => {
+      applyxsl(feedNode, xsl_xml, preview.getElementById("feedBody"), preview);
 
       // replace the content with the preview document
       document.replaceChild(
         document.importNode(preview.documentElement, true),
-        document.documentElement
+        document.documentElement,
       );
 
-      let t0 = performance.now();
+      const _t0 = performance.now();
 
       formatsubtitle();
 
@@ -311,45 +317,37 @@
       extensionimages();
       applysettings();
 
-      let t1 = performance.now();
+      const _t1 = performance.now();
       //console.log("exec in: " + (t1 - t0) + "ms");
 
-      document.title = document.getElementById('feedTitleText').innerText;
+      document.title = document.getElementById("feedTitleText").innerText;
     });
   }
-
 
   function onOptions(opts) {
     options = opts;
 
-    let feedRoot = detect();
+    const feedRoot = detect();
 
     if (feedRoot && !options.preventPreview) {
-
       main(feedRoot);
-    }
-
-    else if (options.doDetect) {
-
+    } else if (options.doDetect) {
       findFeeds();
     }
-
   }
 
   function onError(error) {
     console.log(`Error on get options: ${error}`);
   }
 
-  let getting = browser.storage.sync.get(options);
+  const getting = browser.storage.sync.get(options);
   getting.then(onOptions, onError);
-
 
   function registerFeeds(feeds) {
     if (Object.keys(feeds).length > 0) {
-      function handleResponse(message) {
-      }
+      function handleResponse(_message) {}
 
-      function handleError(error) {
+      function handleError(_error) {
         //console.log(error);
       }
 
@@ -357,25 +355,28 @@
     }
   }
 
-
   function findiTunesPodcastsFeeds() {
-    let match = document.URL.match(/id(\d+)/)
+    var xhr;
+    const match = document.URL.match(/id(\d+)/);
     if (match) {
-      let feeds = {};
-      let itunesid = match[1];
+      const feeds = {};
+      const itunesid = match[1];
 
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', "https://itunes.apple.com/lookup?id="+itunesid+"&entity=podcast");
+      xhr = new XMLHttpRequest();
+      xhr.open(
+        "GET",
+        `https://itunes.apple.com/lookup?id=${itunesid}&entity=podcast`,
+      );
 
-      xhr.onload = function () {
+      xhr.onload = () => {
         if (xhr.readyState === xhr.DONE) {
           if (xhr.status === 200) {
-            let res = JSON.parse(xhr.responseText);
+            const res = JSON.parse(xhr.responseText);
 
             if ("results" in res) {
-              let pod = res["results"][0];
-              let title = pod["collectionName"] || document.title;
-              let url = pod["feedUrl"];
+              const pod = res.results[0];
+              const title = pod.collectionName || document.title;
+              const url = pod.feedUrl;
               if (url) {
                 feeds[url] = title;
               }
@@ -395,16 +396,18 @@
     // and handle links of the form /@MyChannelHandle.
     // Match also on '%' to handle non-latin character codes
     // Match on both of these to autodetect channel feeds on either URL
-    let idPattern = /channel\/([a-zA-Z0-9%_-]+)/;
-    let namePattern = /(?:c|user)\/[a-zA-Z0-9%_-]+/;
-    let handlePattern = /@[a-zA-Z0-9%_-]+/;
-    let urlPattern = new RegExp(`${idPattern.source}|${namePattern.source}|${handlePattern.source}`);
+    const idPattern = /channel\/([a-zA-Z0-9%_-]+)/;
+    const namePattern = /(?:c|user)\/[a-zA-Z0-9%_-]+/;
+    const handlePattern = /@[a-zA-Z0-9%_-]+/;
+    const urlPattern = new RegExp(
+      `${idPattern.source}|${namePattern.source}|${handlePattern.source}`,
+    );
     if (document.URL.match(urlPattern)) {
-      let feeds = {};
-      let canonicalUrl = document.querySelector("link[rel='canonical']").href;
-      let channelId = canonicalUrl.match(idPattern)[1];
-      let url = `https://www.youtube.com/feeds/videos.xml?channel_id=${channelId}`;
-      let title = document.title;
+      const feeds = {};
+      const canonicalUrl = document.querySelector("link[rel='canonical']").href;
+      const channelId = canonicalUrl.match(idPattern)[1];
+      const url = `https://www.youtube.com/feeds/videos.xml?channel_id=${channelId}`;
+      const title = document.title;
       feeds[url] = title;
       registerFeeds(feeds);
     }
@@ -413,20 +416,24 @@
   // The default function used to find feeds if a domain-specific function doesn't exist.
   // Parse the document's HTML looking for link tags pointing to the feed URL.
   function defaultFindFeeds() {
-    let feeds = {};
-    document.querySelectorAll("link[rel='alternate']").forEach( (elem) => {
-      let type_attr = elem.getAttribute('type');
+    const feeds = {};
+    document.querySelectorAll("link[rel='alternate']").forEach((elem) => {
+      const type_attr = elem.getAttribute("type");
       if (!type_attr) {
         return;
       }
 
-      let type = type_attr.toLowerCase();
-      if (type.includes('rss') || type.includes('atom') || type.includes('feed')) {
-        let title = elem.getAttribute('title');
-        let url = elem.href;
+      const type = type_attr.toLowerCase();
+      if (
+        type.includes("rss") ||
+        type.includes("atom") ||
+        type.includes("feed")
+      ) {
+        const title = elem.getAttribute("title");
+        const url = elem.href;
 
         if (url) {
-          feeds[url] = (title ? title : url);
+          feeds[url] = title ? title : url;
         }
       }
     });
@@ -442,8 +449,7 @@
   function findFeeds() {
     // Look up a feed detection function based on the domain.
     // If a domain-specific function doesn't exist, fall back to a default.
-    let finder = domainFeedFinders.get(document.domain) || defaultFindFeeds;
+    const finder = domainFeedFinders.get(document.domain) || defaultFindFeeds;
     finder();
   }
-
 })();
