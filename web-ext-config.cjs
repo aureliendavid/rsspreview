9610337575f559291de680c9b0c67d233de975f6
Aurelien David <aurelien.david@telecom-paristech.fr>
2026-05-15 17:16:33 +0200

    v3.35.1 patch:
    - add webext ignore file

---
+++ web-ext-config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+
+    ignoreFiles: [
+      'web-ext-config.cjs',
+      'todo.txt',
+      'testxsl.html',
+      'package-lock.json',
+      'node_modules/',
+      '**/*.log',
+    ],
+
+};
